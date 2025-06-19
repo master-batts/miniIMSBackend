22bd7e1246bfb8043cacebe1dbffd36b7ef6c6e0
abdelhay <abdelhaay.98@gmail.com>
2025-06-19 23:11:53 +0100

    initial commit

---
+++ src/main/java/com/testproject/miniIMS/MiniImsApplication.java
@@ -0,0 +1,13 @@
+package com.testproject.miniIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniImsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testproject/miniIMS/MiniImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.miniIMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
