151b5f04aa8e154c45296fad32b28f6511518e28
abdelhay <abdelhaay.98@gmail.com>
2025-06-21 00:53:48 +0100

    add ErrorDetails and RessourceNotFoundException

---
+++ src/main/java/com/testproject/miniIMS/exception/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.testproject.miniIMS.exception;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
