d78f828bc0d3f5214d8f26b399a9a18d10268cfe
abdelhay <abdelhaay.98@gmail.com>
2025-06-20 23:35:28 +0100

    add product and category dtos

---
+++ src/main/java/com/testproject/miniIMS/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package com.testproject.miniIMS.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+
+    private Long categoryId;
+    private String categoryName;
+}
