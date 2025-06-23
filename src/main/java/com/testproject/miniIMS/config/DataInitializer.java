b7dfe4c8d7e7c1b84ab64b951176ceab6c0863ee
abdelhay <abdelhaay.98@gmail.com>
2025-06-23 03:22:41 +0100

    add DataInitializer to init roles

---
+++ src/main/java/com/testproject/miniIMS/config/DataInitializer.java
@@ -0,0 +1,28 @@
+package com.testproject.miniIMS.config;
+
+import com.testproject.miniIMS.entity.Role;
+import com.testproject.miniIMS.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) {
+        createRoleIfNotExists("ROLE_USER");
+        //createRoleIfNotExists("ROLE_ADMIN");
+    }
+
+    private void createRoleIfNotExists(String roleName) {
+        if (roleRepository.findByName(roleName) == null) {
+            Role role = new Role();
+            role.setName(roleName);
+            roleRepository.save(role);
+        }
+    }
+}
